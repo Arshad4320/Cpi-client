216a0a2a477f310f2a1e85b7c4ca0ee6728fdec7
Arshad Ullah <mdarshadullah835@gmail.com>
2023-01-21 11:01:13 +0600

    update banner section

--- src/pages/BookList/BookList.js
+++ src/pages/BookList/BookList.js
@@ -10,8 +10,8 @@ const BookList = () => {
             .then(books =>setBooks (books))
     }, []) 
     return (
-        <div >
-            <h2 className='text-center text-4xl my-10 font-bold text-indigo-900'>Semester Book List</h2>
+        <div>
+            <h2 className='text-center text-5xl my-10 font-bold text-indigo-900'>Semester Book List</h2>
             <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ' >
                 {
                     books.map(book => <Book key={book.id} book={book}></Book>)
--- src/pages/Nabvar/Navbar.js
+++ src/pages/Nabvar/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
         <li className='text-lg lg:text-white font-semibold'><Link to='/contact'>Contact</Link></li>
     </>
     return (
-        <div className="navbar bg-color">
+        <div className="navbar bg-color ">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
